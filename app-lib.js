ca10e45af91a99b1fe811fcdbf9d0f164699a8a6
smmadara <smmadara@gmail.com>
2016-03-22 10:49:58 -0400

    3/22/2016
    Cleaned up main.html, Cleaned up app-lib.js and added additional
    variables

--- app-lib.js
+++ app-lib.js
@@ -1,66 +1,86 @@
 //***** Use this file to set up your buttons and include any custom functions for your app *****
 // 
-var development = 1;                         // 1 this app is in development, 0 this app is in production
-var btncount = 2;                            // This is the number of buttons on the App Home tab
-
-//var currentUserDefaultCaution = "0";    // **This is the global Throw caution to the wind flag for the current use
+var development = 1;						// 1 this app is in development, 0 this app is in production
+//var currentUserDefaultCaution = "0";		// **This is the global Throw caution to the wind flag for the current use
 
 /*-----------------------------------------------------*/
 /*-------------- Standard App Variables ---------------*/
 /*-----------------------------------------------------*/
 
-var appname = "appTemplate";                 // This apps name / folder name which must be unique
-var apptitle = "App Template (Edit Me)";     // This display title written to the HTML
-var connect_link = "http://factory.snapback-apps.com/app/new-app-template/";     // The link to your App's Connect Page in the SnapBack Apps Factory
+var appname = "appTemplate";				// This app's name / folder name which must be unique
+var apptitle = "App Template";				// This display title written to the HTML
+var connect_link = "http://factory.snapback-apps.com/app/new-app-template/";     // The link to your App's Connect Page in the SnapBack Apps Factory 
+var group_name ="App Helpers";				// The group or individual responsible for this app
 
 /*-----------------------------------------------------*/
 /*---------------------- Buttons ----------------------*/
 /*-----------------------------------------------------*/
 
+var btncount = 2;                            // This is the number of buttons on the App Home tab in main.html
+
 //Setup for Button0 (The First Button)
-var btn0_Title = "First Button Title";            // This is what is displayed on the button
-var btn0_Description = "First Description: This App displays a message when the user clicks the button on the left.";		// This is what is displayed as the description of the button
-var btn0_Command = "displaymessage0.js";
-var btn0_Source = "displaymessage0.txt";    
-var btn0_CommandParms = "";
-var btn0_id="btn0";
-var btn0_ServiceName="DisplayMessage1";
-var btn0_ElevateNeeded=0;                  	// Needed by service _ in xml file for button
-var btn0_ScriptHasUI=0;                    	// Needed by service _ in xml file for button
-// Permissions for button0 
-var btn0_KeyValue="button0";
+var btn0_Title = "First Button Title";		// This is what is displayed on the button.
+var btn0_MouseOver = "A MouseOver for Button 1";	// This is what appears upon mouseover for the button.
+var btn0_Description = "First Description: This App displays a message when the user clicks the button on the left.";		// This is what is displayed as the description of the button.
+var btn0_Command = "displaymessage0.js";	// This is the actual script the button will be calling.
+var btn0_Source = "displaymessage0.txt";    // Duplicate the above script as a .txt file so that the source code can be shown on the How it Works tab.
+var btn0_CommandParms = "";                 // Add any parameters if and only if your command receives them.
+var btn0_id="btn0";							// The unique ID of the button.  Please follow the format provided.
+
+// Permissions for button0 to be set in HKEY_LOCAL_MACHINE 
+// Don't forget to edit the complementary app-reg.reg file and run it as an administrator
+var btn0_KeyValue="button0"; 				// Key value name for the registry
 var btn0_DefaultEnableButton=1;
-var btn0_CurrentEnableButton=0;        		// Display on how it works _ Admin only can modify Current values 
+var btn0_CurrentEnableButton=1;        		// Display on Permissions tab.  Only an admin can modify current values 
 var btn0_LastModifiedEnableButton="";  		// Update timestamp if admin modifies current value
 var btn0_DefaultAllowUser=1;
-var btn0_CurrentAllowUser=0;         		// Display on how it works _ Admin only can modify Current values 
+var btn0_CurrentAllowUser=1;         		// Display on Permissions tab.  Only an admin can modify current values 
 var btn0_LastModifiedAllowUser="";   		// Update timestamp if admin modifies current value
 var btn0_DefaultCaution=1;
-var btn0_CurrentCaution=0;           		// Display on how it works _ Admin only can modify Current values 
+var btn0_CurrentCaution=1;           		// Display on Permissions tab.  Only an admin can modify current values 
 var btn0_LastModifiedCaution="";     		// Update timestamp if admin modifies current value
-var buttonname0 ="";
+//var buttonname0 ="";
+
+// Service Information for Button0
+// This is used in production only and verified by the Certification Committee
+// Do not edit if you are unfamiliar with the SnapBack Apps Service
+var btn0_ServiceName="";                    // A unique name if using the service.  Need by service in xml file for button 
+var btn0_ElevateNeeded=0;                  	// Does this button need elevation? Needed by service in xml file for button
+var btn0_ScriptHasUI=0;                    	// Does the script have an UI? Needed by service in xml file for button
+
+
+
+
 
 //Setup for Button1 (The Second Button)
-var btn1_Title = "Second Button Title";     // this is what is displayed on the button
-var btn1_Description = "Second Description: This App displays a message when the user clicks the button on the left.";		// This is what is displayed as the description of the button
-var btn1_Command = "displaymessage1.js";
-var btn1_Source = "displaymessage1.txt";
-var btn1_CommandParms = "";
-var btn1_id="btn1";
-var btn1_ServiceName="DisplayMessage2";
-var btn1_ElevateNeeded=0;                  	// Needed by service _ in xml file for button
-var btn1_ScriptHasUI=0;                    	// Needed by service _ in xml file for button    
-// Permissions for button1 
-var btn1_DefaultEnableButton=0;
-var btn1_CurrentEnableButton=0;        		// Display on how it works _ Admin only can modify Current values 
+var btn1_Title = "Second Button Title";		// This is what is displayed on the button.
+var btn1_MouseOver = "A MouseOver for Button 2";	// This is what appears upon mouseover for the button.
+var btn1_Description = "Second Description: This App displays a message when the user clicks the button on the left.";		// This is what is displayed as the description of the button.
+var btn1_Command = "displaymessage1.js";	// This is the actual script the button will be calling.
+var btn1_Source = "displaymessage1.txt";    // Duplicate the above script as a .txt file so that the source code can be shown on the How it Works tab.
+var btn1_CommandParms = "";                 // Add any parameters if and only if your command receives them.
+var btn1_id="btn1";							// The unique ID of the button.  Please follow the format provided.
+
+// Permissions for button1 to be set in HKEY_LOCAL_MACHINE 
+// Don't forget to edit the complementary app-reg.reg file and run it as an administrator
+var btn1_KeyValue="button1"; 				// Key value name for the registry
+var btn1_DefaultEnableButton=1;
+var btn1_CurrentEnableButton=1;        		// Display on Permissions tab.  Only an admin can modify current values 
 var btn1_LastModifiedEnableButton="";  		// Update timestamp if admin modifies current value
-var btn1_DefaultAllowUser=0;
-var btn1_CurrentAllowUser=0;         		// Display on how it works _ Admin only can modify Current values 
+var btn1_DefaultAllowUser=1;
+var btn1_CurrentAllowUser=1;         		// Display on Permissions tab.  Only an admin can modify current values 
 var btn1_LastModifiedAllowUser="";   		// Update timestamp if admin modifies current value
-var btn1_DefaultCaution=0;
-var btn1_CurrentCaution=0;           		// Display on how it works _ Admin only can modify Current values 
+var btn1_DefaultCaution=1;
+var btn1_CurrentCaution=1;           		// Display on Permissions tab.  Only an admin can modify current values 
 var btn1_LastModifiedCaution="";     		// Update timestamp if admin modifies current value
-var buttonname1 ="";
+//var buttonname1 ="";
+
+// Service Information for Button1
+// This is used in production only and verified by the Certification Committee
+// Do not edit if you are unfamiliar with the SnapBack Apps Service
+var btn1_ServiceName="";                    // A unique name if using the service.  Need by service in xml file for button 
+var btn1_ElevateNeeded=0;                  	// Does this button need elevation? Needed by service in xml file for button
+var btn1_ScriptHasUI=0;                    	// Does the script have an UI? Needed by service in xml file for button
 
 
 
--- readme.txt
+++ readme.txt
@@ -17,6 +17,7 @@ Add any notes you may find pertinent
 Changelog
 
 ----
+03/22/2016, smadara, Cleaned up main.html, Cleaned up app-lib.js and added additional variables
 
 03/21/2016, smadara, Totally changed how common areas are embedded into main.html, modifed app-lib.js
 
